fd8514aa430046f6b1558ec2034a2249fd6b02be
ShaikhMR18 <mazharspay@gmail.com>
2022-07-04 10:08:53 +0530

    commit

--- practice/src/App.js
+++ practice/src/App.js
@@ -1,15 +1,21 @@
 import { useState } from "react";
+import './Button';
+import './Display';
 import './App.css';
+import Button from "./Button";
+import Display from "./Display";
 
 function App() {
   const [counter,setCounter]= useState(0);
 
-  const Change=()=>{
-    setCounter(counter+1);
+  const incrementBy = (value)=>{
+    setCounter(counter+parseInt(value));
   }
+  
   return (
     <div className="App">
-      <button onClick={Change}>{counter}</button>
+      <Button incrementBy={incrementBy} />
+      <Display message={counter}/>
     </div>
   );
 }
